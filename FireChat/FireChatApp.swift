97f16e3f332720b36a13a8c3ecab7077b1b15991
Ronak Harkhani <ronak.harkhani@phonepe.com>
2021-04-05 22:02:35 +0530

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Ronak Harkhani on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
